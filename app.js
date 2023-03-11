71f12c37feaab5246285b3ca37c50d9fc770f3b9
JUMTBOX <yjey12@naver.com>
2023-03-11 21:27:23 +0900

    fetch 연습

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = 4000;
 
-app.use("/", (req, res) => {
+app.use(cors());
+
+const userData = [
+  {
+    id: "yjey12",
+    name: "양재연",
+  },
+  {
+    id: "calm",
+    name: "김윤환",
+  },
+  {
+    id: "miso",
+    name: "한두열",
+  },
+];
+
+app.get("/", (req, res) => {
   res.send("hi");
+});
+
+app.get("/users", (req, res) => {
+  console.log(JSON.stringify(userData));
+  res.send(JSON.stringify(userData));
 });
 
 app.listen(PORT, () => {
