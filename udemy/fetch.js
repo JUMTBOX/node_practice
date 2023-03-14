2e54aa901a93a245b63b194a5ba4e6509b9fdb02
JUMTBOX <yjey12@naver.com>
2023-03-14 13:13:29 +0900

    fetch 코드 리팩토링

--- udemy/fetch.js
+++ udemy/fetch.js
@@ -1,7 +1,15 @@
-fetch("https://swapi.dev/api/people/1/")
-  .then((res) => {
-    console.log("resolved!");
-    return res.json();
-  })
-  .then((data) => console.log(data.name))
-  .catch((e) => console.log("ERROR!", e));
+// fetch("https://swapi.dev/api/people/1/")
+//   .then((res) => {
+//     console.log("resolved!");
+//     return res.json();
+//   })
+//   .then((data) => console.log(data.name))
+//   .catch((e) => console.log("ERROR!", e));
+
+async function pp() {
+  let res = await fetch("https://swapi.dev/api/people/1/");
+  let data = await res.json();
+  console.log(data);
+}
+
+pp();
